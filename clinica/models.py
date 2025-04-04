9ace9be0b3648408cc5ea697d5c4e56f184dc358
ramos-emily <ramos.emilyvi@gmail.com>
2025-04-04 15:23:20 -0300

    task 1/6

--- clinica/models.py
+++ clinica/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import RegexValidator, EmailValidator
 
 escolhas = [
     ("CAR", "Cardiologista"),
@@ -15,11 +16,25 @@ estatus = [
 class Medico(models.Model):
     nome = models.CharField(max_length=30, blank=True, null=True)
     especialidade = models.CharField(max_length=3, choices=escolhas)
-    crm = models.CharField(max_length=7)
-    email = models.EmailField(max_length=30, default='DEFAULT VALUE', null=True)
+    crm = models.CharField(
+        max_length=8, 
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[A-Z]{2}/\d{5}$',
+                message="CRM deve estar no formato XX/XXXXX, exemplo: SP/12345."
+            )
+        ]
+    )
+    email = models.EmailField(
+        unique=True,
+        validators=[EmailValidator(message="Insira um e-mail válido.")]
+    )
 
     def __str__(self):
-        return self.nome
+        return f"{self.nome} - {self.crm}"
+    
+
 
 class Consulta(models.Model):
     paciente = models.CharField(max_length=30)
