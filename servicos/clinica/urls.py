4451e1d3074e4a7db939cc8bf6c31e12c9fe0cbc
ramos-emily <ramos.emilyvi@gmail.com>
2025-04-04 14:22:24 -0300

    task 1/3

--- servicos/clinica/urls.py
+++ servicos/clinica/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/medicos/', view=views.listar_medicos, name='listar_medicos'),
-    path('/consultas/nova/', view=views.criar_consulta, name='criar_consulta'),
-    path('/consultas/<int:id>/', view=views.detalhes_consulta, name='detalhes_consulta'),
+    path('medicos/', view=views.listar_medicos, name='listar_medicos'),
+    path('consultas/nova/', view=views.criar_consulta, name='criar_consulta'),
+    path('consultas/<int:id>/', view=views.detalhes_consulta, name='detalhes_consulta'),
 ]
