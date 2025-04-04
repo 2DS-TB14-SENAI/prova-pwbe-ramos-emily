8fd0102a84efc39e7156561dac997681b46a2254
ramos-emily <ramos.emilyvi@gmail.com>
2025-04-04 14:11:08 -0300

    task 1/1

---
+++ servicos/clinica/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('/medicos/', view=views.listar_medicos, name='listar_medicos'),
+    path('/consultas/nova/', view=views.criar_consulta, name='criar_consulta'),
+    path('/consultas/<int:id>/', view=views.detalhes_consulta, name='detalhes_consulta'),
+]
---
+++ servicos/clinica/views.py
@@ -0,0 +1,33 @@
+from .models import *
+from .serializers import MedicoSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+#Lista todos os médicos cadastrados
+@api_view(['GET'])
+def listar_medicos(request):
+    medicos = Medico.objects.all()
+    serializer = MedicoSerializer(medicos, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+#Formulário para agendar nova consulta
+@api_view(['POST'])
+def criar_consulta(request):
+    serializer = MedicoSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#Exibe informações de uma consulta específica
+@api_view(['GET'])
+def detalhes_consulta(request):
+    medico = get_object_or_404(Medico)
+    serializer = MedicoSerializer(medico)
+    return Response(serializer.data, status=status.HTTP_200_OK)
